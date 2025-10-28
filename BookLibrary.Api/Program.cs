0b069524db211d0b32f6f1cf03f5a31d86db1f63
iachb <ivanchbrb@gmail.com>
2025-10-27 18:57:59 -0600

    Configuring Swagger for endpoints documentation

--- BookLibrary.Api/Program.cs
+++ BookLibrary.Api/Program.cs
@@ -1,11 +1,17 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "BookLibrary", Version = "v1" });
+});
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -13,7 +19,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "BookLibrary");
+    });
 }
 
 app.UseHttpsRedirection();
