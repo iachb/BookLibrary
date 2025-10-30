355b852b65efb1a29c11a37f5b68fdeaa9172af3
iachb <ivanchavezbrb@gmail.com>
2025-10-29 19:08:10 -0600

    Adding required fields

--- BookLibrary.Api/Models/Books/BookDTO.cs
+++ BookLibrary.Api/Models/Books/BookDTO.cs
@@ -1,10 +1,15 @@
-﻿namespace BookLibrary.Api.Models.Books
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookLibrary.Api.Models.Books
 {
     public sealed class BookDTO
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; } = null!;
+        [Required]
         public DateTime PublishedDate { get; set; }
+        [Required]
         public string? AuthorName { get; set; } = string.Empty;
     }
 }
