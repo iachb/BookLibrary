5b21e725f999fc678c8b076067d5e31d1bf29152
iachb <ivanchavezbrb@gmail.com>
2025-12-09 20:12:33 -0600

    Get all authors service, returns author item list

---
+++ BookLibrary.Tests/Services/AuthorServiceTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using BookLibrary.Core.Entities;
+using BookLibrary.Core.Interfaces.Repository;
+using BookLibrary.Core.Models.Authors;
+using BookLibrary.Core.Models.Books;
+using BookLibrary.Infrastructure.Services;
+using Moq;
+using Xunit;
+
+namespace BookLibrary.Tests.Services
+{
+    public class AuthorServiceTests
+    {
+        private readonly Mock<IMapper> _mapperMock;
+        private readonly Mock<IAuthorRepository> _repositoryAuthorMock;
+        public AuthorServiceTests()
+        {
+            _repositoryAuthorMock = new Mock<IAuthorRepository>();
+            _mapperMock = new Mock<IMapper>();
+        }
+
+        [Fact]
+        public async Task GetAllAuthorsAsync_ReturnsIReadOnlyList()
+        {
+            // Arrange
+            var cancellationToken = new CancellationToken();
+
+            var authorList = new List<TAuthor>
+            {
+                new TAuthor {
+                    Id = 1, 
+                    Name = "AuthorName", 
+                    BirthDate = DateTime.Now, 
+                    Books = {
+                        new TBook { 
+                            Id = 1, 
+                            Title = "BookName" } 
+                    } 
+                },
+                new TAuthor {
+                    Id = 2,
+                    Name = "AnotherAuthorName",
+                    BirthDate = DateTime.Now,
+                    Books = {
+                        new TBook {
+                            Id = 2,
+                            Title = "AnotherBookName" }
+                    }
+                },
+            };
+
+            var authorListItem = new List<AuthorItem>
+            {
+                new AuthorItem {
+                    Id = 1,
+                    Name = "AuthorName",
+                    BirthDate = DateTime.Now,
+                    Books = {
+                        new BookItem {
+                            Id = 1,
+                            Title = "BookName" }
+                    }
+                },
+                new AuthorItem {
+                    Id = 2,
+                    Name = "AnotherAuthorName",
+                    BirthDate = DateTime.Now,
+                    Books = {
+                        new BookItem {
+                            Id = 2,
+                            Title = "AnotherBookName" }
+                    }
+                },
+            } as IReadOnlyList<AuthorItem>;
+
+            _repositoryAuthorMock.Setup(a => a.GetAllAuthorsAsync(cancellationToken))
+                .ReturnsAsync(authorList);
+            _mapperMock.Setup(m => m.Map<IReadOnlyList<AuthorItem>>(authorList))
+                .Returns(authorListItem);
+
+            var authorService = new AuthorService(_repositoryAuthorMock.Object, _mapperMock.Object);
+
+            // Act
+            var result = await authorService.GetAllAuthorsAsync(cancellationToken);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(authorListItem, result);
+            _repositoryAuthorMock.Verify(r => r.GetAllAuthorsAsync(cancellationToken), Times.Once);
+            _mapperMock.Verify(m => m.Map<IReadOnlyList<AuthorItem>>(authorList), Times.Once);
+        }
+    }
+}
